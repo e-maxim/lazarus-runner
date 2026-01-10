17b8a2a9b3edaeb536ad168391947dee69f4af35
Maksim Emeliashin <max.emeliashin@xmail.ru>
2026-01-10 19:19:35 +0700

    refactoring (remove function)

--- sources/utils.sh
+++ sources/utils.sh
@@ -12,18 +12,6 @@ function is_system_dir(){
     return 1
 }
 
-function clean_dir(){
-    local target_dir="$1"
-    if is_system_dir "$target_dir"; then
-        echo "❌ Refusing to clean protected system directory: $target_dir"
-        return 1
-    fi
-    if [[ -d "$target_dir" ]]; then
-        rm -rf "$target_dir"/* "$target_dir"/.[!.]* "$target_dir"/..?*
-    fi
-    return $?
-}
-
 function delete_dir(){
     local target_dir="$1"
     if is_system_dir "$target_dir"; then
@@ -48,7 +36,7 @@ function git_sync_dir(){
     local repo_url="$2"
     local branch="$3"
     if [[ ! -d "$target_dir/.git" ]]; then
-        clean_dir "$target_dir"
+        delete_dir "$target_dir"
         git clone --branch "$branch" "$repo_url" "$target_dir"
     else
         pushd $(pwd) >/dev/null
